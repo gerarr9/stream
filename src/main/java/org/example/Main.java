b02db205a423d9819d71f12ca21b676e47fcc2bf
321ru <gerarr999@yandex.ru>
2023-01-28 19:22:29 +0300

    work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Main<T> {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>();
+        nums.add(13);
+        nums.add(12);
+        nums.add(11);
+        nums.add(55);
+        nums.add(22);
+
+        nums.stream().map(num -> {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            return null;
+        }).collect(Collectors.toList());
     }
 }
